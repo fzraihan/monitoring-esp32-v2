7f7b05ad8e596783dc947809c08fd7cf4cbd687d
fzraihan <faizzraihan5@gmail.com>
2026-03-02 08:04:18 +0700

    Refactor Supabase data retrieval with dynamic limit

--- ambil_data_supabase.php
+++ ambil_data_supabase.php
@@ -1,19 +1,25 @@
 <?php
-
 header('Content-Type: application/json');
 
-$supabaseUrl = "https://fykjfyieanedfjrctrdi.supabase.co/rest/v1/data_sensor?select=*&order=id.desc&limit=50";
+$supabaseUrl = "https://fykjfyieanedfjrctrdi.supabase.co/rest/v1/data_sensor";
 $supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5a2pmeWllYW5lZGZqcmN0cmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMDYxMDksImV4cCI6MjA4Nzg4MjEwOX0.-_HYcQXNrj065l7GO-uRkTviTPv0caYaqNZsfl1aDfQ";
 
-$options = [
+// Ambil limit dari parameter GET, default 50
+$limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 50;
+
+$opts = [
     "http" => [
-        "method"  => "GET",
-        "header"  => "apikey: $supabaseKey\r\n" .
-                     "Authorization: Bearer $supabaseKey\r\n"
+        "method" => "GET",
+        "header" => "apikey: $supabaseKey\r\nAuthorization: Bearer $supabaseKey\r\n"
     ]
 ];
 
-$context = stream_context_create($options);
-$response = file_get_contents($supabaseUrl, false, $context);
+$context = stream_context_create($opts);
+$result = file_get_contents(
+    $supabaseUrl."?select=*&order=created_at.desc&limit=".$limit,
+    false,
+    $context
+);
 
-echo $response;
+echo $result;
+?>
